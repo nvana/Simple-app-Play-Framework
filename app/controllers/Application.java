08142ff45902dbb96adb13d33f59946ff5301a71
oria22 <oriana.meril@gmail.com>
2014-02-12 22:56:42 +0100

    Calcul du total
    calcul total sans les controles

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -24,14 +24,14 @@ public class Application extends Controller {
          public Integer sept; 
 
          /* Cartes pour l'atout */
-        @Required public Integer as;
-         @Required public Integer dix;
-        public Integer roi;
-        public Integer dame;
-         public Integer valet;
-         public Integer neuf;
-         public Integer huit;
-         public Integer sept; 
+        @Required public Integer asA;
+         @Required public Integer dixA;
+        public Integer roiA;
+        public Integer dameA;
+         public Integer valetA;
+         public Integer neufA;
+         public Integer huitA;
+         public Integer septA; 
         
     } 
     
@@ -56,7 +56,7 @@ public class Application extends Controller {
         } else {
             Hello data = form.get();
             return ok(
-                hello.render(data.as*11+data.dix*10+data.roi*4+data.dame*3+data.valet*2)
+                hello.render(data.as*11+data.dix*10+data.roi*4+data.dame*3+data.valet*2+data.asA*11+data.dixA*10+data.roiA*4+data.dameA*3+data.valetA*20+data.neufA*14)
             );
         }
     }
